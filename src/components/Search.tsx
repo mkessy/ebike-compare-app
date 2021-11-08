e44b06b4e4acdbf5f01ab2703db660498270169b
mkessy <kokokessy@gmail.com>
2021-11-08 15:04:51 -0800

    Added search list component, tabs

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,14 +1,63 @@
-import React, { useState, useMemo, useCallback } from "react";
-import { TextField, Box } from "@mui/material";
+import React, { useState, useMemo, useContext, useCallback } from "react";
+import GlobalContext from "../hooks/useGlobalContext";
+import {
+  TextField,
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemButton,
+} from "@mui/material";
 
-const Search: React.FunctionComponent = () => {
+export const Search: React.FunctionComponent = () => {
   const [search, setSearch] = useState("");
+  const [selectedIdx, setSelectedIdx] = useState<null | number>(null);
+  const bikes = useContext(GlobalContext).bikes;
+
+  const searchResults = useMemo(() => {
+    return bikes
+      .filter((bike) => {
+        const {
+          modelBrand: { manufacturer, model },
+        } = bike;
+        return (
+          manufacturer.toLowerCase().includes(search.toLowerCase()) ||
+          model.toLowerCase().includes(search.toLowerCase())
+        );
+      })
+      .slice(0, 20);
+  }, [bikes, search]);
+
+  const handleClickSearchItem = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    index: number
+  ) => {
+    setSelectedIdx(index);
+  };
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+    setSelectedIdx(null);
+  };
 
   return (
     <Box>
-      <TextField variant="filled" fullWidth onChange={() => {}} />
+      <TextField variant="filled" fullWidth onChange={handleSearch} />
+      <List dense={true}>
+        {searchResults.map((bike, index) => {
+          return (
+            <ListItemButton
+              onClick={(event) => handleClickSearchItem(event, index)}
+              selected={index === selectedIdx}
+            >
+              <ListItemText
+                primary={bike.modelBrand.model}
+                secondary={bike.modelBrand.manufacturer}
+              />
+            </ListItemButton>
+          );
+        })}
+      </List>
     </Box>
   );
 };
-
-export default Search;
--- src/components/SearchCompareTabs.tsx
+++ src/components/SearchCompareTabs.tsx
@@ -1,6 +1,7 @@
 import { Tab } from "@mui/material";
 import { Box } from "@mui/system";
-
+import { EbikeForm } from "./EbikeForm";
+import { Search } from "./Search";
 import { TabContext, TabList, TabPanel } from "@mui/lab";
 import React from "react";
 
@@ -11,18 +12,23 @@ const SearchCompareTabs = () => {
     setValue(newValue);
   };
   return (
-    <Box>
-      <TabContext value={value}>
-        <Box>
-          <TabList aria-label="Search and Compare tabs" onChange={handleChange}>
-            <Tab label="Search" value="search" />
-            <Tab label="Compare" value="compare" />
-          </TabList>
-        </Box>
-        <TabPanel value="search">Search</TabPanel>
-        <TabPanel value="compare">Compare</TabPanel>
-      </TabContext>
-    </Box>
+    <TabContext value={value}>
+      <TabList
+        centered
+        aria-label="Search and Compare tabs"
+        onChange={handleChange}
+      >
+        <Tab label="Search" value="search" />
+        <Tab label="Compare" value="compare" />
+      </TabList>
+
+      <TabPanel value="search">
+        <Search />
+      </TabPanel>
+      <TabPanel value="compare">
+        <EbikeForm />
+      </TabPanel>
+    </TabContext>
   );
 };
 
